dd84374c02c5343aba8386cf2b6ab38966a9f675
Farhod <offorwork23@gmail.com>
2025-11-01 12:18:21 +0500

    update api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,1 +1,2 @@
-export const baseURL = `${window.location.protocol}//${window.location.hostname}:5050/api`;
+// export const baseURL = `${window.location.protocol}//${window.location.hostname}:5050/api`;
+export const baseURL = `http://192.168.100.103:8888/api`;
